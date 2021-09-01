5c82c6238879ae5b1141417a3a97a865c4d502dd
rainerAme <89884817+rainerAme@users.noreply.github.com>
2021-09-01 12:07:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const app = Vue.createApp({
         },
         stage2: {
           q1: 'いいい',
-           q2: '愛',
+           //q2: '愛',
           // q3: 'おおお'
         },
         stage3: {
@@ -41,7 +41,7 @@ const app = Vue.createApp({
         ],
         stage2: [
           false, // 2-1
-           false, // 2-2
+           //false, // 2-2
           // false, // 2-3
         ],
         stage3: [
